cb3a438f6a68bed0209d80a58e0908f04e38a3cb
hrzx201_git122232400 <qq122232400@github.com>
2021-07-24 00:54:39 +0800

    hrz_202100724_a1

---
+++ daycode/d21720.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main() {
+	int i=1;
+	int j=1;
+	int res=-1;
+	res = i & j;
+	
+	byte a=0000;
+
+	printf("res=%d\n",res);
+
+
+}
---
+++ daycode/d21722.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+	
+//	int new_value=-164;
+//	int new_result = new_value >> 2;
+//	printf("new_result:%d\n",new_result);
+	
+	printf("\n**********");
+	
+	for(int i=1;i<=8;i++) {
+		printf("\n*        *");
+	}	
+	printf("\n**********");
+	return 0;
+
+}
+
+	
+
+
+
+
+
+
+
+
+
+
