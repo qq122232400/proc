94080240ea4c060a7c5eb64d8ceb646e622072bf
hrzx201_git122232400 <qq122232400@github.com>
2021-07-31 00:46:31 +0800

    hrz_20210731_01

--- daycode/d210716.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("210716.\n");
-	int i=1;
-	i=i<<1;
-	printf("i=%d\n",i);
-
-}
--- daycode/day20210423.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-	printf("the os is hrz_x201_ubuntu.\n");
-	printf("\tTitle		\tContent \n");
-	printf("\tA		\table \n");
-
-
-}
